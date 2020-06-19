ad171525a73292fe3e64b24d8c1d0ff397a0ee8e
Vinny V <VinAVarghese@gmail.com>
2020-06-18 21:30:59 -0700

    set up done. functionality almost complete. Just need to work on the localstorage and recall aspects

--- script.js
+++ script.js
@@ -1,9 +1,63 @@
 // Grabbing HTML //
 var currentDay = $("#currentDay")
+var nine = $(".nineAm")
+var ten = $(".tenAm")
+var eleven = $(".elevenAm")
+var twelve = $(".twelveAm")
+var one = $(".onePm")
+var two = $(".twoPm")
+var three = $(".threePm")
+var four = $(".fourPm")
+var five = $(".fivePm")
+var nineBtn = $(".nineBtn")
+var tenBtn = $(".tenBtn")
+var elevenBtn = $(".elevenBtn")
+var twelveBtn = $(".twelveBtn")
+var oneBtn = $(".oneBtn")
+var twoBtn = $(".twoBtn")
+var threeBtn = $(".threeBtn")
+var fourBtn = $(".fourBtn")
+var fiveBtn = $(".fiveBtn")
+var allBtn = $(".saveBtn")
 
-// Setting Variables //
-var today = moment().format("dddd") 
 
+// Global Arrays // 
+var hours = [9,10,11,12,13,14,15,16,17]
+var hoursElArray = [nine,ten,eleven,twelve,one,two,three,four,five]
+
+// Today's Date //
 $(currentDay).text(moment().format("dddd, MMMM Do"))
+
+// Checking Past,Present,Future Hours For Class Additions //
+var currentHour = moment().hour() 
+for (let i = 0; i < 9; i++) {
+    if (currentHour === hours[i]){
+        $(hoursElArray[i]).addClass("present")
+    } else if (currentHour > hours[i]) {
+        $(hoursElArray[i]).addClass("past")
+    } else {
+        $(hoursElArray[i]).addClass("future")
+    }
+}
+
+// EventListeners For Saving //
+$(allBtn).click(function(){
+    var saveThis = $(this).siblings(".description").val();
+    localStorage.setItem("userData",saveThis);
+    localStorage.getItem("userData");
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
